bcdcfb78cd65f6dee114ac97108ee13568eb7220
Cem Üstün <ustuncem7@gmail.com>
2021-07-09 13:17:07 +0300

    Add project files.

---
+++ wwwroot/js/common.js
@@ -0,0 +1,7 @@
+(function($) {
+    "use strict"; // Start of use strict  
+    
+    //load common parts
+    $("#accordionSidebar").load("./../common/sidebar.html");  
+})(jQuery); // End of use strict
+  
