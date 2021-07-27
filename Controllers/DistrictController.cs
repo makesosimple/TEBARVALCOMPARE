bc9b1f5273b59ae6827deee0507276b843ad5c64
Cem Üstün <ustuncem7@gmail.com>
2021-07-27 12:43:25 +0300

    District (Without Select2)

---
+++ Controllers/DistrictController.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using IBBPortal.Data;
+using IBBPortal.Models;
+
+namespace IBBPortal.Controllers
+{
+    public class DistrictController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DistrictController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: District
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public JsonResult JSONData(string creationDate, string updateDate)
+        {
+            try
+            {
+
+                var draw = HttpContext.Request.Query["draw"].FirstOrDefault();
+                // Skiping number of Rows count  
+                var start = Request.Query["start"].FirstOrDefault();
+                // Paging Length 10,20  
+                var length = Request.Query["length"].FirstOrDefault();
+                // Sort Column Name  
+                var sortColumn = Request.Query["columns[" + Request.Query["order[0][column]"].FirstOrDefault() + "][name]"].FirstOrDefault();
+                // Sort Column Direction ( asc ,desc)  
+                var sortColumnDirection = Request.Query["order[0][dir]"].FirstOrDefault();
+                // Search Value from (Search box)  
+                var searchValue = Request.Query["search[value]"].FirstOrDefault();
+
+                //Paging Size (10, 20, 50,100)  
+                int pageSize = length != null ? Convert.ToInt32(length) : 0;
+                int skip = start != null ? Convert.ToInt32(start) : 0;
+                int recordsTotal = 0;
+
+                var data = from x in _context.District select x;
+
+                //Sorting  
+                if (!(string.IsNullOrEmpty(sortColumn) && string.IsNullOrEmpty(sortColumnDirection)))
+                {
+                    var searchProp = sortColumn + " " + sortColumnDirection;
+                    data = data.OrderBy(e => e.DistrictName);
+                }
+
+                //Search  
+                if (!string.IsNullOrEmpty(searchValue))
+                {
+                    data = data.Where(m => m.DistrictName.Contains(searchValue));
+                }
+
+                //total number of rows count   
+                recordsTotal = data.Count();
+                //Paging   
+                var passData = data.ToList();
+
+                var test = HttpContext.Request.Query;
+                //Returning Json Data  
+                return Json(new { draw = draw, recordsFiltered = recordsTotal, recordsTotal = recordsTotal, data = passData });
+
+            }
+
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        // GET: District/Details/5
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contractor = _context.District
+                .Where(m => m.DistrictID == id).FirstOrDefault();
+            if (contractor == null)
+            {
+                return NotFound();
+            }
+
+            return PartialView("_DetailsModal", contractor);
+        }
+
+        // GET: District/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: District/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("DistrictID,DistrictCode,DistrictName,CityID,CreationDate,UpdateDate,DeletionDate")] District district)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(district);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(district);
+        }
+
+        // GET: District/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var district = await _context.District.FindAsync(id);
+            if (district == null)
+            {
+                return NotFound();
+            }
+            return View(district);
+        }
+
+        // POST: District/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("DistrictID,DistrictCode,DistrictName,CityID,CreationDate,UpdateDate,DeletionDate")] District district)
+        {
+            if (id != district.DistrictID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(district);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DistrictExists(district.DistrictID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(district);
+        }
+
+        // GET: District/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var district = await _context.District
+                .FirstOrDefaultAsync(m => m.DistrictID == id);
+            if (district == null)
+            {
+                return NotFound();
+            }
+
+            return PartialView("_DeleteModal", district);
+        }
+
+        // POST: District/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var district = await _context.District.FindAsync(id);
+            _context.District.Remove(district);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool DistrictExists(int id)
+        {
+            return _context.District.Any(e => e.DistrictID == id);
+        }
+    }
+}
