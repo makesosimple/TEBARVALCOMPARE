0d6a4e33836d11f70211ccf09171835255cb79fa
cemresa <l6A19049@std.yildiz.edu.tr>
2021-08-02 13:32:31 +0300

    Added ProjectTeamCategory

--- Models/ProjectTeamCategory.cs
+++ Models/ProjectTeamCategory.cs
@@ -4,29 +4,32 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace IBBPortal.Models
 {
 
-    
+    [Index(nameof(UserID))]
     public class ProjectTeamCategory
     {   
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int ProjectTeamCategoryID { get; set; }
 
+        [Required]
         [MaxLength(50)]
         public string ProjectTeamCategoryTitle { get; set; }
         [MaxLength(50)]
-        public string ProjectTeamCategoryDescription { get; set; }
+        public string? ProjectTeamCategoryDescription { get; set; }
+        public string UserID { get; set; }
+        [ForeignKey("UserID")]
 
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public IdentityUser User { get; set; }
+
         public DateTime CreationDate { get; set; }
 
-        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
         public DateTime? UpdateDate { get; set; }
 
-        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
         public DateTime? DeletionDate { get; set; }
 
     }
