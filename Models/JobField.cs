c798e01d535232503eb915fd3e148cca25f8854e
Cem Üstün <ustuncem7@gmail.com>
2021-08-12 13:52:17 +0300

    Added Models For ProjectPerson

---
+++ Models/JobField.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IBBPortal.Models
+{
+    public class JobField
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int JobFieldID { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string JobFieldTitle { get; set; }
+
+        [MaxLength(256)]
+        public string? JobFieldDescription { get; set; }
+
+        public string UserID { get; set; }
+        [ForeignKey("UserID")]
+        public ApplicationUser User { get; set; }
+
+        [Required]
+        public DateTime CreationDate { get; set; }
+
+        public DateTime? UpdateDate { get; set; }
+
+        public DateTime? DeletionDate { get; set; }
+    }
+}
---
+++ Models/ProjectPerson.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IBBPortal.Models
+{
+    public class ProjectPerson
+    {
+        public int ProjectPersonID { get; set; }
+
+        public int JobFieldID { get; set; }
+
+        public int ProjectID { get; set; }
+
+        public int ContractorID { get; set; }
+
+        public int JobTitleID { get; set; }
+
+        public string? ProjectPersonDescription { get; set; }
+
+        public bool IsInternal { get; set; }
+
+        public string UserID { get; set; }
+        [ForeignKey("UserID")]
+        public ApplicationUser User { get; set; }
+
+        [Required]
+        public DateTime CreationDate { get; set; }
+
+        public DateTime? UpdateDate { get; set; }
+
+        public DateTime? DeletionDate { get; set; }
+    }
+}
