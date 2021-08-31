0f2f4fdb967398d1aefc082e85e92c2f6b5dc1cc
makesosimple <so@makesosimple.com>
2021-08-31 12:40:14 +0300

    dashboard-wip-31

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,6 +3,11 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using IBBPortal.Data;
+using System.Threading.Tasks;
+using IBBPortal.ViewModels;
 
 namespace IBBPortal.Controllers
 {
@@ -10,14 +15,25 @@ namespace IBBPortal.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context, UserManager<ApplicationUser> userManager)
         {
             _logger = logger;
+            _context = context;
+            _userManager = userManager;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            var dashboardSummary = await _context.DashboardSummaryModel.FromSqlRaw("SELECT (SELECT COUNT(ProjectID) FROM Project) AS NumberOfProjects, (SELECT COUNT(ProjectPhaseID) FROM ProjectPhase WHERE ProjectPhase.ProjectPhaseStart >= DATEADD(day, -30, GETDATE()) AND ProjectPhase.ProjectPhaseStart <= GETDATE()) AS ProjectsStartedInLastMonth, ((SELECT COUNT(ProjectID) FROM Project WHERE ProjectStatusID = 10) / (SELECT COUNT(ProjectID) FROM Project)) AS NumberOfCompletedProjects").FirstOrDefaultAsync();
+
+            //Console.Writeln(dashboardSummary.Result.NumberOfProjects);
+            //var numberOfProjects = await _context.Project.CountAsync();
+            ViewBag.numberOfProjects = System.Convert.ToInt32(dashboardSummary.NumberOfProjects);
+
+
             return View();
         }
 
---
+++ ViewModels/DashboardSummaryModel.cs
@@ -0,0 +1,15 @@
+﻿using IBBPortal.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IBBPortal.ViewModels
+{
+    public class DashboardSummaryModel
+    {
+        public Int32 NumberOfProjects { get; set; }
+        public Int32 ProjectsStartedInLastMonth { get; set; }
+        public Int32 NumberOfCompletedProjects { get; set; }
+    }
+}
