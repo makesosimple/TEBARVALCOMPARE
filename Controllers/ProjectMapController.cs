4327799c089e34d466d882f7846416b1ad9ff6c9
makesosimple <so@makesosimple.com>
2021-09-09 14:19:09 +0300

    project-detail

--- Controllers/ProjectMapController.cs
+++ Controllers/ProjectMapController.cs
@@ -26,17 +26,40 @@ namespace IBBPortal.Controllers
             return View();
         }
 
-        // GET: ProjectDetail/5
+        // GET: https://localhost:44360/ProjectMap/ProjectDetail/?projectID=168
         public async Task<IActionResult> ProjectDetail(int? projectID)
         {
 
-            var projectField = await _context.ProjectField
-                .Include(m => m.Project.ProjectTitle)
+            if (projectID == null)
+            {
+                return NotFound();
+            }
+
+            /*var projectField = await _context.ProjectField
+                //.Include(m => m.Project.ProjectTitle)
                 .Include(m => m.coordinates)
-                .Include(m => m.Project.ProjectIBBCode)
+                //.Include(m => m.Project.ProjectIBBCode)
                 .Include(m => m.ProjectLatitude)
                 .Include(m => m.ProjectLongitude)
-                .FirstOrDefaultAsync(m => m.ProjectID == projectID);
+                .FirstOrDefaultAsync(m => m.ProjectID == projectID);*/
+
+            var projectDetail = await _context.ProjectField.Select(m => new
+            {
+                ProjectTitle = m.Project.ProjectTitle,
+                m.ProjectID,
+                m.ProjectLatitude,
+                m.ProjectLongitude,
+                m.coordinates,
+            }).FirstOrDefaultAsync(m => m.ProjectID == projectID);
+
+            if (projectDetail == null)
+            {
+                return NotFound();
+            }
+
+            ViewBag.projectDetail = projectDetail;
+            //ViewBag.projectField = projectField;
+
             return View();
         }
     }
